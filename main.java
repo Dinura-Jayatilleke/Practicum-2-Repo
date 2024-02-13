13f3c42d12cd6629d1581a64f5802dcd7997d5c6
Dinura Jayatilleke <you@example.com>
2024-02-13 07:36:56 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 // public static void main in Java
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
     int n = 1;
@@ -9,7 +9,7 @@ public class Main {
     while(n <= 10) {
       sum = sum + n;
 
-      if(args.length != 0 &&  /* TO COMPLETE */) {
+      if(args.length != 0 && args.length <= sum  /* TO COMPLETE */) {
         System.out.println("n = " + n);
         System.out.println("sum = " + sum);
       }
